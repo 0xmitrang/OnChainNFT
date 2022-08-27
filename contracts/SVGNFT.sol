ade5729ee0676166a87a1576e39dedc92df9cfd3
mitrang <mitrang.lad@smartsensesolutions.com>
2022-08-27 13:28:41 +0530

    SVGNFT.sol contract

---
+++ contracts/SVGNFT.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/base64.sol";
+
+contract SVGNFT is ERC721URIStorage {
+    uint256 public tokenCounter;
+
+    event CreatedSVGNFT(uint256 indexed tokenId, string tokenURI);
+
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        tokenCounter = 0;
+    }
+
+    function create(string memory _svg) public {
+        string memory imageURI = svgToImageURL(_svg);
+        string memory tokenURI = formatTokenURI(imageURI);
+        _safeMint(msg.sender, tokenCounter);
+        _setTokenURI(tokenCounter, tokenURI);
+        emit CreatedSVGNFT(tokenCounter, tokenURI);
+        tokenCounter++;
+    } 
+
+    function svgToImageURL(string memory _svg) public pure returns(string memory) {
+        return string(abi.encodePacked("data:image/svg+xml;base64,", Base64.encode(bytes(string(abi.encodePacked(_svg))))));
+    }
+
+    function formatTokenURI(string memory _imageURI) public pure returns (string memory) {
+        return string(abi.encodePacked(
+            "data:application/svg+xml;base64,",
+            Base64.encode(bytes(
+                abi.encodePacked(' {"name":"SVG NFT", "description": "SVG based NFT!", "attributes": "", "image": "', _imageURI, '"}')
+            ))
+        ));
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {version: "0.8.1"},
+      {version: "0.8.0"},
+      {version: "0.7.0"},
+      {version: "0.6.6"},
+      {version: "0.4.24"}
+    ]
+  }
 };
